add53e705775a4b3b4ef2f4936a021c9e1e6d695
Shivesh0333 <68682235+Shivesh0333@users.noreply.github.com>
2020-10-01 23:33:25 +0530

    Update even odd.cpp

--- even odd.cpp
+++ even odd.cpp
@@ -1,19 +1,14 @@
-#include<stdio.h>
-#include<math.h>
+#include<bits/stdc++.h>
+using namespace std;
 int main()
 {
 	char a;
-	scanf("%c",&a);
+	cin>>a;
 	if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u' || a=='A' || a=='O' || a=='U' || a=='E' || a=='I')
-	{
-		printf("VOWEL");
-	}
-	else
-	{
-		if(a>=48 && a<=57)
-			printf("digit");
-		else
-			printf("consonant");
+	cout<<"VOWEl";
+	else{
+		if(a>=48&&a<=57) cout<<"Digit";
+		else cout<<"CONSTANT OR CHARACTER";
 	}
 	return 0;
 }
